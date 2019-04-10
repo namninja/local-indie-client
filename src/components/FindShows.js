75b138da6baca0180f79eba40a9962c7b5705180
Nam Ninja <namninja@gmail.com>
2019-04-09 22:11:32 -0700

    Created empty component files

--- src/components/FindShows.js
+++ src/components/FindShows.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class FindShows extends Component {
   render() {
-    return <div />;
+    return (
+      <div className="search-show">
+        <h1>Find a show</h1>
+        <form>
+          <label />
+        </form>
+      </div>
+    );
   }
 }
 
