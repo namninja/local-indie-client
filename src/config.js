4dd68a11ffa9ce0c00fa319066c1934b98f35915
Nam Ninja <namninja@gmail.com>
2019-04-23 18:53:53 -0700

    deploying to heroku

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080"
+  API_BASE_URL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://stormy-hollows-11476.herokuapp.com"
 };
